f4822b56b146f2656addb5cb69c3508dbc54e93d
Kodavali841 <kodavali.1@iitj.ac.in>
2021-05-15 18:14:34 +0530

    changes test.py

--- times/blog/tests.py
+++ times/blog/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from . models import Post
+from django.contrib.auth.models import User
 
 
 class ModelTesting(TestCase):
 
     def setUp(self):
-        self.blog = Post.objects.create(title='django', author='django', content = "This is for testing")
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+        self.blog = Post.objects.create(title='django', author=test_user1, content = "This is for testing")
 
     def test_post_model(self):
         d = self.blog
